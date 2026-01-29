3181a29627f0f98cfbccd672fd30e73e71a11d1e
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-01-29 21:11:59 +0000

    style(ui): align game over screen

--- src/engine/Component_228.ts
+++ src/engine/Component_228.ts
@@ -1,3 +1,2 @@
-// Engine sub-system edit seq: 228
-import { Vector2 } from './math';
-export class Entity_228 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
+// Engine sub-system edit seq: 628
+export function detectCollision(a, b) { return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.h + a.y > b.y; }
