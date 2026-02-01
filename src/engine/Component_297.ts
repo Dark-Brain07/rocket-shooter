6218b76ad8bd664af262847d7c05ee3a669d7dfd
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-01 18:47:22 +0000

    chore(audio): configure sfx manager

--- src/engine/Component_297.ts
+++ src/engine/Component_297.ts
@@ -1,2 +1,3 @@
-// Engine sub-system edit seq: 297
-export const GAME_CONSTANTS = { GRAVITY: 9.8, MAX_SPEED: 15, MAX_HEALTH: 100 };
+// Engine sub-system edit seq: 697
+import { Vector2 } from './math';
+export class Entity_297 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
