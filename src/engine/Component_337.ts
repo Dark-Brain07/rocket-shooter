a5f1372fb7b5a09263b0c08563ed760de2557e9c
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-03 08:31:09 +0000

    style(input): fix styling for gamepad support

--- src/engine/Component_337.ts
+++ src/engine/Component_337.ts
@@ -1,2 +1,2 @@
-// Engine sub-system edit seq: 337
+// Engine sub-system edit seq: 737
 export function detectCollision(a, b) { return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.h + a.y > b.y; }
