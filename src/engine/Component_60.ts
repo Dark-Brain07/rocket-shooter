74caa66192d7485f9caa65043f6b111bbfeb1355
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-08 23:02:17 +0000

    feat(audio): support sfx manager

--- src/engine/Component_60.ts
+++ src/engine/Component_60.ts
@@ -1,3 +1,3 @@
-// Engine sub-system edit seq: 460
+// Engine sub-system edit seq: 860
 import { Vector2 } from './math';
 export class Entity_60 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
