17bf344ad96f3f2b8b9639c894c83348c97c0706
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-12 03:05:04 +0000

    feat(levels): add wave parser

--- src/engine/Component_121.ts
+++ src/engine/Component_121.ts
@@ -1,3 +1,3 @@
-// Engine sub-system edit seq: 521
+// Engine sub-system edit seq: 921
 import { Vector2 } from './math';
 export class Entity_121 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
