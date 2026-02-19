7c3d21c9d249635dafffcac73f05bd5275ed4497
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-19 10:23:58 +0000

    test(entities): add tests for spawn logic

--- src/engine/Component_272.ts
+++ src/engine/Component_272.ts
@@ -1,2 +1,2 @@
-// Engine sub-system edit seq: 672
+// Engine sub-system edit seq: 1072
 export function detectCollision(a, b) { return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.h + a.y > b.y; }
