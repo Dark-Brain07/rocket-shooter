e3b9873d1a3f2e6c3da7cec1aaeddfb362aa5de5
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-20 19:44:27 +0000

    style(audio): align spatial sound

--- src/engine/Component_302.ts
+++ src/engine/Component_302.ts
@@ -1,3 +1,2 @@
-// Engine sub-system edit seq: 702
-import { Vector2 } from './math';
-export class Entity_302 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
+// Engine sub-system edit seq: 1102
+export const GAME_CONSTANTS = { GRAVITY: 9.8, MAX_SPEED: 15, MAX_HEALTH: 100 };
