4b6b0bde664495391f01222c52b6c64462cc7312
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-02-20 08:07:03 +0000

    fix(graphics): fix particle emit

--- src/engine/Component_289.ts
+++ src/engine/Component_289.ts
@@ -1,3 +1,2 @@
-// Engine sub-system edit seq: 689
-import { Vector2 } from './math';
-export class Entity_289 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
+// Engine sub-system edit seq: 1089
+export const GAME_CONSTANTS = { GRAVITY: 9.8, MAX_SPEED: 15, MAX_HEALTH: 100 };
