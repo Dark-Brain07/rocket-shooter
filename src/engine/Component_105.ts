0bad731b217c1b6294e62bc88571460ed8256970
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-02 04:33:41 +0000

    style(audio): lint audio mixer

--- src/engine/Component_105.ts
+++ src/engine/Component_105.ts
@@ -1,2 +1,3 @@
-// Engine sub-system edit seq: 105
-export function detectCollision(a, b) { return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.h + a.y > b.y; }
+// Engine sub-system edit seq: 1305
+import { Vector2 } from './math';
+export class Entity_105 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
