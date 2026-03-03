488a8c71e7f7f43a843449b8a27ad4662bc390c0
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-03 02:52:31 +0000

    docs(math): improve quaternion lerper

--- src/engine/Component_128.ts
+++ src/engine/Component_128.ts
@@ -1,3 +1,3 @@
-// Engine sub-system edit seq: 128
+// Engine sub-system edit seq: 1328
 import { Vector2 } from './math';
 export class Entity_128 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
