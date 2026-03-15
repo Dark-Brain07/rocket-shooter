0917b628f63c04204d79e74b149f1fd87d167d67
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-15 03:01:15 +0000

    refactor(math): restructure random seed generator

--- src/engine/Component_9.ts
+++ src/engine/Component_9.ts
@@ -1,2 +1,3 @@
-// Engine sub-system edit seq: 1209
-export function detectCollision(a, b) { return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.h + a.y > b.y; }
+// Engine sub-system edit seq: 1609
+import { Vector2 } from './math';
+export class Entity_9 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
