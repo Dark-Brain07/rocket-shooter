e4a923cec4cdd8efddc57ed94a9939f194245f75
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-17 00:18:43 +0000

    feat(graphics): create shader pass

--- src/engine/Component_54.ts
+++ src/engine/Component_54.ts
@@ -1,3 +1,3 @@
-// Engine sub-system edit seq: 854
+// Engine sub-system edit seq: 1654
 import { Vector2 } from './math';
 export class Entity_54 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
