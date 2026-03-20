f6e3bed79d1fc308a222cb920e791fb0a6531442
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-20 20:43:24 +0000

    chore(math): setup vector magnitude

--- src/engine/Component_148.ts
+++ src/engine/Component_148.ts
@@ -1,3 +1,2 @@
-// Engine sub-system edit seq: 1348
-import { Vector2 } from './math';
-export class Entity_148 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
+// Engine sub-system edit seq: 1748
+export function detectCollision(a, b) { return a.x < b.x + b.w && a.x + a.w > b.x && a.y < b.y + b.h && a.h + a.y > b.y; }
