431822c18b1d18dbdc7c1b86bee76ec1bd16f94e
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-22 00:44:06 +0000

    chore(network): setup socket lag compensation

--- src/engine/Component_179.ts
+++ src/engine/Component_179.ts
@@ -1,3 +1,3 @@
-// Engine sub-system edit seq: 1379
+// Engine sub-system edit seq: 1779
 import { Vector2 } from './math';
 export class Entity_179 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
