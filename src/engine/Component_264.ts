70b4ba5e65ba6ad65d1818aaec27d15976ff8671
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-26 12:41:17 +0000

    refactor(entities): restructure player movement

--- src/engine/Component_264.ts
+++ src/engine/Component_264.ts
@@ -1,3 +1,2 @@
-// Engine sub-system edit seq: 1464
-import { Vector2 } from './math';
-export class Entity_264 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
+// Engine sub-system edit seq: 1864
+export const GAME_CONSTANTS = { GRAVITY: 9.8, MAX_SPEED: 15, MAX_HEALTH: 100 };
