fbb1b7115a48309254c9179232bc8f6c28a00865
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-27 22:15:18 +0600

    feat: add LRUCache data structure

---
+++ src/utils/structures/LRUCache.ts
@@ -0,0 +1,6 @@
+export class LRUCache<T=any>{private data:T[]=[];
+add(item:T){this.data.push(item);}
+remove():T|undefined{return this.data.pop();}
+get size(){return this.data.length;}
+clear(){this.data=[];}
+isEmpty():boolean{return this.data.length===0;}}
