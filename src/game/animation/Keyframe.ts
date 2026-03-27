999f3a5d06e8646d53f4285867da035b5cd87930
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-27 22:14:43 +0600

    feat: add Keyframe module

---
+++ src/game/animation/Keyframe.ts
@@ -0,0 +1,8 @@
+export class Keyframe{private playing=false;private time=0;
+play(){this.playing=true;}
+pause(){this.playing=false;}
+stop(){this.playing=false;this.time=0;}
+update(dt:number){if(this.playing)this.time+=dt;}
+getTime():number{return this.time;}
+isPlaying():boolean{return this.playing;}
+reset(){this.time=0;this.playing=false;}}
