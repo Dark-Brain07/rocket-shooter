494ab59f173a72025566244bb656a90b403623c0
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-03-31 10:43:54 +0000

    feat(config): support game constants

--- src/engine/Component_379.ts
+++ src/engine/Component_379.ts
@@ -1,3 +1,3 @@
-// Engine sub-system edit seq: 1179
+// Engine sub-system edit seq: 1979
 import { Vector2 } from './math';
 export class Entity_379 { constructor(x, y) { this.pos = new Vector2(x, y); } update() {} }
